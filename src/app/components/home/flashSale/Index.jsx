e8e26c8917fc7dfddca021540a363ab23f68fcfc
mostofa zaman <mostofazaman1234@gmail.com>
2026-04-05 23:48:49 +0600

    feat: add ProductSkeleton component for loading state UI

--- src/app/components/home/flashSale/Index.jsx
+++ src/app/components/home/flashSale/Index.jsx
@@ -4,6 +4,7 @@ import Container from '../../common/Container';
 import Heading from '../../common/Heading';
 import Timer from '../../common/Timer';
 
+
 const FlashSale = () => {
   return (
     <div className='mt-[140px] pb-[60px]'>
@@ -11,6 +12,7 @@ const FlashSale = () => {
         <div className='flex items-end gap-x-[87px]'>
           <Heading title="tomorrow" description="Flash Sale" />
           <Timer  targetDate="2026-04-10T00:00:00"/>
+         
         </div>
       </Container>
     </div>
