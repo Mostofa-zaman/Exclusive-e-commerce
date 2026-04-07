d6c9a785408d651616929309ac9774c673b83390
mostofa zaman <mostofazaman1234@gmail.com>
2026-04-07 23:56:49 +0600

    fix cart image ,button & bacground

--- src/app/components/common/ProductCard.jsx
+++ src/app/components/common/ProductCard.jsx
@@ -8,9 +8,9 @@ const ProductCard = () => {
   const { gammingImage } = allImages;
   const { eye, navIcon, star } = allIcon;
   return (
-    <div className="group w-[280px] ">
-      <div className="bg-secondary  overflow-hidden  rounded-[4px] py-[75px]   flex justify-center relative">
-        <div className="px-3 py-1 bg-secondary2 text-text absolute rounded-[4px] top-3 left-3  ">
+    <div className="group w-[280px]  ">
+      <div className="bg-[#F5F5F5]  overflow-hidden  rounded-[4px] py-[75px]   flex justify-center relative">
+        <div className="px-3 py-1 bg-[#DB4444] text-white absolute rounded-[4px] top-3 left-3  ">
           -41%
         </div>
         <div className="flex h-fit  flex-col absolute right-[17px] gap-y-2  top-[17px]">
@@ -21,9 +21,9 @@ const ProductCard = () => {
             {eye}
           </button>
         </div>
-        <figure className="relative h-[180px] w-[190px]">
+        <figure className="relative h-[180px] w-[190px] ">
           <Image
-            className="object-cover"
+            className="object-contain"
             src={gammingImage}
             alt="gammingImage"
             fill
@@ -31,11 +31,13 @@ const ProductCard = () => {
           />
         </figure>
 
-<button className="absolute w-full cursor-pointer duration-700 ease-in-out 
--bottom-[18%] group-hover:bottom-0 
-py-[10px] bg-button group-hover:bg-black text-primary group-hover:text-white title16PXMedium">
-  Add To Cart
-</button>
+        <button
+          className="absolute w-full cursor-pointer duration-700 ease-in-out 
+          -bottom-[18%] group-hover:bottom-0 
+             py-[10px] bg-button group-hover:bg-black text-primary group-hover:text-white title16PXMedium"
+        >
+          Add To Cart
+        </button>
       </div>
       <div className="mt-4 space-y-2">
         <div>
