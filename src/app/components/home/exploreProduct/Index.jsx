61d7227477d69b013efadbe551ac35551a23cc6b
mostofa zaman <mostofazaman1234@gmail.com>
2026-04-11 23:56:52 +0600

    feat: add ExploreProduct section with product list and view all button

---
+++ src/app/components/home/exploreProduct/Index.jsx
@@ -0,0 +1,33 @@
+
+"use client";
+
+import React from "react";
+import ProductCommonLayout from "../../common/ProductCommonLayout";
+import Container from "../../common/Container";
+import ProductCard from "../../common/ProductCard";
+import { useGetAllProductQuery } from "../../../features/api/ProductApi";
+import Button from "../../common/Button";
+
+const ExploreProduct = () => {
+  const { data, error, isLoading } = useGetAllProductQuery();
+  return (
+    <Container>
+      <div>
+        <ProductCommonLayout
+          ProductCard={ProductCard}
+          heading="Our Products"
+          description="Explore Our Products"
+          isArrowsTrue={true}
+          componentData={data?.products}
+          rows={2}
+          isLoading={isLoading}
+        />
+        <div className="pb-20  mx-auto  w-58.5  ">
+          <Button children={"View All Products"} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default ExploreProduct;
