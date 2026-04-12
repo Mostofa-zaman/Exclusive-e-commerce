8cac5370493ac70fc4bee6293ce24d7f837a486a
mostofa zaman <mostofazaman1234@gmail.com>
2026-04-12 23:57:38 +0600

    feat: add layout structure for new arrival left image section

--- src/app/components/home/newArrival/Index.jsx
+++ src/app/components/home/newArrival/Index.jsx
@@ -4,21 +4,23 @@ import React from "react";
 import Container from "../../common/Container";
 import Heading from "../../common/Heading";
 import newArrivalLeftImage from "../../../assets/playstaatio.png";
+import Image from "next/image";
 
 const NewArrival = () => {
   return (
     <div>
       <Container>
         <Heading title={"Featured"} description={"New Arrival"} />
+
         <div className="flex justify-between h-[600px] mt-[60px]">
           <div className="w-[58%] bg-black  h-full">
-            <img
+            <Image
               src={newArrivalLeftImage}
-              alt={newArrivalLeftImage}
-              className="w-full h-full o rounded-sm"
+              alt="New Arrival"
+              fill
+              className="w-full h-full object-cover rounded-sm"
             />
           </div>
-          
         </div>
       </Container>
     </div>
