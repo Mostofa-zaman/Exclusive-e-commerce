24aeb18b0e26126743065c7b541cfbe84ef0ae48
mostofa zaman <mostofazaman1234@gmail.com>
2026-05-20 23:57:20 +0600

    fix responsive layout for reusable button component

--- src/app/components/common/Button.jsx
+++ src/app/components/common/Button.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const Button = ({onclick,children,className}) => {
+const Button = ({ onclick, children, className }) => {
   return (
-    <button onClick={onclick} className={` cursor-pointer py-4 px-12 w-full rounded-[6px] text-white bg-[#DB4444]  ${className}`}>{children}</button>
+    <button
+      onClick={onclick}
+      className={`
+        cursor-pointer
+        py-3 md:py-4
+        px-6 md:px-12
+        w-full md:w-auto
+        rounded-[6px]
+        text-white
+        bg-[#DB4444]
+        text-sm md:text-base
+        ${className}
+      `}
+    >
+      {children}
+    </button>
   )
 }
 
